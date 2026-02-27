9175318deb2b02d8d02dab9770aa075352065e17
SenneDerom <senne.derom@student.ucll.be>
2026-02-27 23:22:29 +0100

    added TodoSerice

--- Backend/src/main/java/service/TodoService.java
+++ Backend/src/main/java/service/TodoService.java
@@ -1,4 +1,13 @@
 package service;
 
+import org.springframework.stereotype.Service;
+import repository.TodoRepository;
+
+@Service
 public class TodoService {
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
 }
