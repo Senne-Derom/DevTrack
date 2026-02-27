6c27074fa3b559e076964783297c7e9ea4113e63
SenneDerom <senne.derom@student.ucll.be>
2026-02-27 23:17:17 +0100

    started Todo model

--- Backend/src/main/java/model/Todo.java
+++ Backend/src/main/java/model/Todo.java
@@ -1,4 +1,37 @@
 package model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String name;
+    private String description;
+
+    public Todo(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
