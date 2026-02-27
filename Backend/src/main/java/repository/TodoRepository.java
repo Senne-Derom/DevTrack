5ef186bea553ea30975f75e2fb33538c345c174c
SenneDerom <senne.derom@student.ucll.be>
2026-02-27 23:22:09 +0100

    added TodoRepository

--- Backend/src/main/java/repository/TodoRepository.java
+++ Backend/src/main/java/repository/TodoRepository.java
@@ -1,4 +1,8 @@
 package repository;
 
-public interface TodoRepository {
+
+import model.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
 }
