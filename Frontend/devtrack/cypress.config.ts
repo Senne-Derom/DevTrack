dc228f5365ecff66420cac551d8d0e4d49358fe9
Senne-Derom <senne.derom@gmail.com>
2026-08-18 19:26:34 +0200

    added cypress configuration

---
+++ Frontend/devtrack/cypress.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "cypress";
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import { createEsbuildPlugin } from "@badeball/cypress-cucumber-preprocessor/esbuild";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: process.env.CYPRESS_BASE_URL,
+    specPattern: "**/*.feature",
+    supportFile: "cypress/support/e2e.ts",
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on(
+          "file:preprocessor",
+          createBundler({
+            plugins: [createEsbuildPlugin(config)],
+          })
+      );
+
+      return config;
+    },
+  },
+});
