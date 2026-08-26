9ba7c38be02d85cb55031a7bca5dadc3be962da6
Senne-Derom <senne.derom@gmail.com>
2026-08-26 22:38:45 +0200

    added users

--- Backend/src/main/java/com/devtrack/DbInitializer.java
+++ Backend/src/main/java/com/devtrack/DbInitializer.java
@@ -2,10 +2,13 @@ package com.devtrack;
 
 import com.devtrack.model.Course;
 import com.devtrack.model.StudyEntry;
+import com.devtrack.model.User;
 import com.devtrack.repository.CourseRepository;
 import com.devtrack.repository.StudyEntryRepository;
+import com.devtrack.repository.UserRepository;
 import jakarta.annotation.PostConstruct;
 import jakarta.transaction.Transactional;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -14,10 +17,14 @@ import java.time.LocalDate;
 public class DbInitializer {
     private final CourseRepository courseRepository;
     private final StudyEntryRepository studyEntryRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public DbInitializer(CourseRepository courseRepository, StudyEntryRepository studyEntryRepository) {
+    public DbInitializer(CourseRepository courseRepository, StudyEntryRepository studyEntryRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.courseRepository = courseRepository;
         this.studyEntryRepository = studyEntryRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostConstruct
@@ -33,5 +40,16 @@ public class DbInitializer {
         studyEntryRepository.save(new StudyEntry((Course) courseRepository.findByName("Programming 1").orElseThrow(), "Completed exercises from class", 1, LocalDate.now()));
         studyEntryRepository.save(new StudyEntry((Course) courseRepository.findByName("Server and System Management").orElseThrow(), "Made DNS assignment", 2.5, LocalDate.now()));
         studyEntryRepository.save(new StudyEntry((Course) courseRepository.findByName("Software Engineering").orElseThrow(), "Implemented Flyway", 2, LocalDate.now()));
+
+        User user1 = new User("John Doe", "johndoe", passwordEncoder.encode("John1234"));
+        User user2 = new User("Jane Smith", "janesmith", passwordEncoder.encode("Jane1234"));
+        userRepository.save(user1);
+        userRepository.save(user2);
+        course1.setUser(user1);
+        course2.setUser(user1);
+        course3.setUser(user2);
+        courseRepository.save(course1);
+        courseRepository.save(course2);
+        courseRepository.save(course3);
     }
 }
