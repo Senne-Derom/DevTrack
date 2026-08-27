7b623ab942dd05f0a255f98c217209321c694295
Senne-Derom <senne.derom@gmail.com>
2026-08-27 19:53:19 +0200

    add User cleanup

--- Backend/src/test/java/com/devtrack/DbCleaner.java
+++ Backend/src/test/java/com/devtrack/DbCleaner.java
@@ -2,11 +2,15 @@ package com.devtrack;
 
 import com.devtrack.repository.CourseRepository;
 import com.devtrack.repository.StudyEntryRepository;
+import com.devtrack.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DbCleaner {
+    @Autowired
+    private UserRepository userRepository;
+
     @Autowired
     private CourseRepository courseRepository;
 
@@ -19,11 +23,13 @@ public class DbCleaner {
     public void cleanDatabase() {
         studyEntryRepository.deleteAll();
         courseRepository.deleteAll();
+        userRepository.deleteAll();
         dbInitializer.initialize();
     }
 
     public void emptyDatabase() {
         studyEntryRepository.deleteAll();
         courseRepository.deleteAll();
+        userRepository.deleteAll();
     }
 }
